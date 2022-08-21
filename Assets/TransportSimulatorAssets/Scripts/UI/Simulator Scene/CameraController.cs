1d6a1e8c25dcbe514e491ecb73fc8108f9894ea0
peinz <philippHeinz66@gmail.com>
2022-08-21 19:57:48 +0200

    deactivate everything useing the old input system

--- Assets/TransportSimulatorAssets/Scripts/UI/Simulator Scene/CameraController.cs
+++ Assets/TransportSimulatorAssets/Scripts/UI/Simulator Scene/CameraController.cs
@@ -29,13 +29,13 @@ using UnityEngine.UI;
 /// </summary>
 public class CameraController : MonoBehaviour
 {
-	public GameObject crossHair;
+    public GameObject crossHair;
     public GameObject FlightModeInformation;
     public GameObject MouseModeInformation;
     public GameObject MouseClickDisabler;
     public GameObject InputFieldSearchBar;
     public GameObject SearchDropDown;
-	
+
     public float speedH = 2.0f;
     public float speedV = 2.0f;
 
@@ -45,12 +45,12 @@ public class CameraController : MonoBehaviour
     float mainSpeed = 100.0f; // normal movement speed.
     float shiftAdd = 250.0f; // acceleration speed.
     float maxShift = 1000.0f; // upper speed limit.
-    
-	private float totalRun = 1.0f;
 
-	Camera cam;
+    private float totalRun = 1.0f;
 
-    bool stopmoving = true; 
+    Camera cam;
+
+    bool stopmoving = true;
 
     private void Start()
     {
@@ -59,104 +59,104 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            // While the Instructions Menu is open, it is not possible to switch modes.
-            if (IngameMenu.InstructionsOn)
-            {
-                return;
-            }
+        // if (Input.GetKeyDown(KeyCode.Tab))
+        // {
+        //     // While the Instructions Menu is open, it is not possible to switch modes.
+        //     if (IngameMenu.InstructionsOn)
+        //     {
+        //         return;
+        //     }
 
-            stopmoving = !stopmoving;
-			
-            // Cursor Mode
-            if (stopmoving)
-            {
-                InputFieldSearchBar.gameObject.SetActive(true);
-                if (!IngameMenu.MenuHidden)
-                {
-                    InputFieldSearchBar.transform.GetComponentInParent<InputField>().text = "";
-                }
-                Cursor.visible = true;
-                crossHair.SetActive(false);
-                FlightModeInformation.SetActive(false);
-                MouseModeInformation.SetActive(true);
-                MouseClickDisabler.SetActive(false);
-            }
+        //     stopmoving = !stopmoving;
 
-            // Flight Mode
-            if (!stopmoving)
-            {
-                InputFieldSearchBar.gameObject.SetActive(false);
-                SearchDropDown.gameObject.SetActive(false);
-                Cursor.visible = false;
-                crossHair.SetActive(true);
-                FlightModeInformation.SetActive(true);
-                MouseModeInformation.SetActive(false);
-                MouseClickDisabler.SetActive(true);
-            }
-        }
+        //     // Cursor Mode
+        //     if (stopmoving)
+        //     {
+        //         InputFieldSearchBar.gameObject.SetActive(true);
+        //         if (!IngameMenu.MenuHidden)
+        //         {
+        //             InputFieldSearchBar.transform.GetComponentInParent<InputField>().text = "";
+        //         }
+        //         Cursor.visible = true;
+        //         crossHair.SetActive(false);
+        //         FlightModeInformation.SetActive(false);
+        //         MouseModeInformation.SetActive(true);
+        //         MouseClickDisabler.SetActive(false);
+        //     }
 
-        // Controlls within the flight mode.
-        if(stopmoving == false)
-        {
-            yaw += speedH * Input.GetAxis("Mouse X");
-            pitch -= speedV * Input.GetAxis("Mouse Y");
+        //     // Flight Mode
+        //     if (!stopmoving)
+        //     {
+        //         InputFieldSearchBar.gameObject.SetActive(false);
+        //         SearchDropDown.gameObject.SetActive(false);
+        //         Cursor.visible = false;
+        //         crossHair.SetActive(true);
+        //         FlightModeInformation.SetActive(true);
+        //         MouseModeInformation.SetActive(false);
+        //         MouseClickDisabler.SetActive(true);
+        //     }
+        // }
 
-            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        // // Controlls within the flight mode.
+        // if(stopmoving == false)
+        // {
+        //     yaw += speedH * Input.GetAxis("Mouse X");
+        //     pitch -= speedV * Input.GetAxis("Mouse Y");
 
-            Vector3 p = GetBaseInput();
+        //     transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
 
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                totalRun += Time.deltaTime;
-                p = p * totalRun * shiftAdd;
-                p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
-                p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
-                p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
-            }
-            else
-            {
-                totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
-                p = p * mainSpeed;
-            }
+        //     Vector3 p = GetBaseInput();
 
-            p = p * Time.deltaTime;
-            Vector3 newPosition = transform.position;
-            if (Input.GetKey(KeyCode.Space))
-            {
-                transform.Translate(p);
-                newPosition.x = transform.position.x;
-                newPosition.z = transform.position.z;
-                transform.position = newPosition;
-            }
-            else if (Input.GetKey(KeyCode.X))
-            {
-                transform.Translate(p);
-                newPosition.y = transform.position.y;
-                transform.position = newPosition;
-            }
-            else
-            {
-                transform.Translate(p);
-            }
-        }
+        //     if (Input.GetKey(KeyCode.LeftShift))
+        //     {
+        //         totalRun += Time.deltaTime;
+        //         p = p * totalRun * shiftAdd;
+        //         p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
+        //         p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
+        //         p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
+        //     }
+        //     else
+        //     {
+        //         totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+        //         p = p * mainSpeed;
+        //     }
 
-        // Interacting with the Station Objects.
-        if(stopmoving == true)
-        {         
-            if (Input.GetMouseButtonDown(0))
-            {
-                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-                if (Physics.Raycast(ray, out hit))
-                {
-                    var canvas = hit.transform.GetChild(0).gameObject;
-                    canvas.SetActive(true);
-                    canvas.transform.GetChild(6).gameObject.SetActive(true);
-                }
-            }        
-        }
+        //     p = p * Time.deltaTime;
+        //     Vector3 newPosition = transform.position;
+        //     if (Input.GetKey(KeyCode.Space))
+        //     {
+        //         transform.Translate(p);
+        //         newPosition.x = transform.position.x;
+        //         newPosition.z = transform.position.z;
+        //         transform.position = newPosition;
+        //     }
+        //     else if (Input.GetKey(KeyCode.X))
+        //     {
+        //         transform.Translate(p);
+        //         newPosition.y = transform.position.y;
+        //         transform.position = newPosition;
+        //     }
+        //     else
+        //     {
+        //         transform.Translate(p);
+        //     }
+        // }
+
+        // // Interacting with the Station Objects.
+        // if(stopmoving == true)
+        // {         
+        //     if (Input.GetMouseButtonDown(0))
+        //     {
+        //         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        //         RaycastHit hit;
+        //         if (Physics.Raycast(ray, out hit))
+        //         {
+        //             var canvas = hit.transform.GetChild(0).gameObject;
+        //             canvas.SetActive(true);
+        //             canvas.transform.GetChild(6).gameObject.SetActive(true);
+        //         }
+        //     }        
+        // }
     }
 
     /// <summary>
@@ -166,22 +166,22 @@ public class CameraController : MonoBehaviour
     private Vector3 GetBaseInput()
     {
         Vector3 p_Velocity = new Vector3();
-        if (Input.GetKey(KeyCode.W))
-        {
-            p_Velocity += new Vector3(0, 0, 1);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            p_Velocity += new Vector3(0, 0, -1);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            p_Velocity += new Vector3(-1, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            p_Velocity += new Vector3(1, 0, 0);
-        }
+        // if (Input.GetKey(KeyCode.W))
+        // {
+        //     p_Velocity += new Vector3(0, 0, 1);
+        // }
+        // if (Input.GetKey(KeyCode.S))
+        // {
+        //     p_Velocity += new Vector3(0, 0, -1);
+        // }
+        // if (Input.GetKey(KeyCode.A))
+        // {
+        //     p_Velocity += new Vector3(-1, 0, 0);
+        // }
+        // if (Input.GetKey(KeyCode.D))
+        // {
+        //     p_Velocity += new Vector3(1, 0, 0);
+        // }
         return p_Velocity;
     }
 }
