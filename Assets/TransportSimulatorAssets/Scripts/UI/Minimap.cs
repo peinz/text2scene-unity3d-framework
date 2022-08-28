84c5dd0a909b5061dcac96bcb969e00dc5986217
peinz <philippHeinz66@gmail.com>
2022-08-28 13:12:15 +0200

    add minimap

---
+++ Assets/TransportSimulatorAssets/Scripts/UI/Minimap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap: MonoBehaviour
+{
+    [SerializeField] Transform player;
+
+    void LateUpdate(){
+        // position
+        var newPosition = transform.position;
+        newPosition.x = player.position.x;
+        newPosition.z = player.position.z;
+        transform.position = newPosition;
+
+        // rotation
+        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+    }
+}
