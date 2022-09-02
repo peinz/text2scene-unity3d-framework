a47381e75bd7b4c2c0cf3379ed48669595a51ed1
peinz <philippHeinz66@gmail.com>
2022-09-03 01:41:54 +0200

    add player animations

--- Assets/TransportSimulatorAssets/Scripts/PlayerXRRigSync.cs
+++ Assets/TransportSimulatorAssets/Scripts/PlayerXRRigSync.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 using Unity.Netcode;
 
 
+[RequireComponent(typeof(Animator))]
 public class PlayerXRRigSync : NetworkBehaviour
 {
+    Animator animator;
+
+    NetworkVariable<int> animationState = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animationState.OnValueChanged += OnAnimatorStateChanged;
+    }
+
     void Update()
     {
         if(!IsOwner) return;
@@ -14,13 +25,25 @@ public class PlayerXRRigSync : NetworkBehaviour
 
         var cameraTransform = XRRig.transform.GetChild(0).GetChild(0);
 
-        transform.position = XRRig.transform.position;
+        var velocity = XRRig.transform.position - transform.position;
+        transform.position += velocity;
         transform.eulerAngles = new Vector3(0, cameraTransform.eulerAngles.y, 0);
+
+        // animation
+        if(velocity.sqrMagnitude < 0.1) animationState.Value = 0;
+        else animationState.Value = Vector3.Dot(velocity, cameraTransform.position) > 0 ? 1 : -1;
 
         // deactivate renderer on owner, to prevent seeing it on own screen
         var meshRenders = GetComponentsInChildren<SkinnedMeshRenderer>();
         foreach(var meshRenderer in meshRenders){
             meshRenderer.enabled = false;
+        }
+    }
+
+    void OnAnimatorStateChanged(int previous, int current)
+    {
+        if(animator){
+            animator.SetInteger("state", current);
         }
     }
 }
