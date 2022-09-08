9cd3a05b3deeb4f67e4f0991b1b1aab01fb1a682
peinz <philippHeinz66@gmail.com>
2022-09-08 15:58:09 +0200

    add script to set server ip

--- Assets/TransportSimulatorAssets/Scripts/NetworkOrchestrator.cs
+++ Assets/TransportSimulatorAssets/Scripts/NetworkOrchestrator.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
 using Unity.Netcode;
 using System.Collections;
+using Unity.Netcode.Transports.UTP;
 
 [RequireComponent(typeof(NetworkManager))]
 public class NetworkOrchestrator : MonoBehaviour
 {
+    public string ServerIp = "127.0.0.1";
+    public ushort ServerPort = 7777;
     NetworkManager networkManager;
 
     void Start()
     {
         networkManager = GetComponent<NetworkManager>();
         networkManager.OnServerStarted += OnServerStarted;
+
+        networkManager.GetComponent<UnityTransport>().SetConnectionData(
+            ServerIp,  // The IP address is a string
+            ServerPort,  // The port number is an unsigned short
+            "0.0.0.0"
+        );
     }
 
     void OnServerStarted()
